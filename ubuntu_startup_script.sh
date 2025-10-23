ef012642da79e03a42202ede809e6d374313d7d6
Abdul Jaseem VP <67306692+vpjaseem@users.noreply.github.com>
2025-10-23 13:20:29 +0530

    Create ubuntu_startup_script.sh

---
+++ ubuntu_startup_script.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+sudo apt update -y
+sudo apt install -y apache2 php libapache2-mod-php unzip wget
+
+sudo systemctl enable --now apache2
+
+sudo chmod -R 777 /var/www/html
+
+cd /tmp
+wget -O main.zip "https://github.com/vpjaseem/az-lin-php-web-app/archive/refs/heads/main.zip"
+unzip -o main.zip
+sudo mv az-lin-php-web-app-main/* /var/www/html/
+rm -rf az-lin-php-web-app-main main.zip
+
+cd /var/www/html/
+rm -r index.html
+
+sudo systemctl restart apache2
